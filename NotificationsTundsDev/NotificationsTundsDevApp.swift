63ae5daf7c1acf3b93aa46251ca1d575cf7494ae
shrekle <nanonline760@gmail.com>
2023-05-08 09:07:27 -0800

    Initial Commit

---
+++ NotificationsTundsDev/NotificationsTundsDevApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotificationsTundsDevApp.swift
+//  NotificationsTundsDev
+//
+//  Created by adrian garcia on 5/8/23.
+//
+
+import SwiftUI
+
+@main
+struct NotificationsTundsDevApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
